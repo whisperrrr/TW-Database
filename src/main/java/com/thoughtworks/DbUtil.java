79b9d9657cc802ae9bcbe2bd180ef2701aacad3b
JinHui <jh731448520@163.com>
2020-03-27 21:49:49 +0800

    [JinHui]{fix}-{#2-1}:close resourse and extract method

--- src/main/java/com/thoughtworks/DbUtil.java
+++ src/main/java/com/thoughtworks/DbUtil.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class StudentUtil {
+public class DbUtil {
     public static final String URL = "jdbc:mysql://localhost:3306/student_manage_sys?useUnicode=true&characterEncoding=utf-8&serverTimezone=Hongkong";
     public static final String USER = "root";  // MySQL里面Ctrl+U进行查看
     public static final String PASSWORD = "931205";
@@ -14,8 +14,6 @@ public class StudentUtil {
         try {
             //1.加载驱动程序
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // 【deprecated】
-            // Class.forName("com.mysql.jdbc.Driver");
             //2. 获得数据库连接
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
@@ -24,7 +22,7 @@ public class StudentUtil {
             e.printStackTrace();
         }
     }
-    
+
     public static Connection getConnection() {
         return conn;
     }
--- src/main/java/com/thoughtworks/StudentRepository.java
+++ src/main/java/com/thoughtworks/StudentRepository.java
@@ -21,68 +21,49 @@ public class StudentRepository {
 
     public void save(Student student) throws SQLException {
         // 获取连接
-        Connection connection = StudentUtil.getConnection();
+        Connection connection = DbUtil.getConnection();
         // sql
         String sql = "INSERT INTO studentjdbc(id, name, sex, school_time, birthday, class)"
                 + "VALUES (?, ?, ?, ?, ?, ?);";
         // 预编译
-        PreparedStatement ptmt = connection.prepareStatement(sql); //预编译SQL，减少sql执行
-        // 传参
-        ptmt.setString(1, student.getId());
-        ptmt.setString(2, student.getName());
-        ptmt.setString(3, student.getGender());
-        ptmt.setInt(4, student.getAdmissionYear());
-        ptmt.setDate(5, new Date(student.getBirthday().getTime())); // 两个Date不一样
-        ptmt.setString(6, student.getClassId());
+        try (PreparedStatement ptmt = connection.prepareStatement(sql)) {
+            // 传参
+            ptmt.setString(1, student.getId());
+            ptmt.setString(2, student.getName());
+            ptmt.setString(3, student.getGender());
+            ptmt.setInt(4, student.getAdmissionYear());
+            ptmt.setDate(5, new Date(student.getBirthday().getTime())); // 两个Date不一样
+            ptmt.setString(6, student.getClassId());
 
-        // 执行
-        ptmt.execute();
+            // 执行
+            ptmt.executeUpdate();
+        }
     }
 
     public List<Student> query() throws SQLException {
-        Connection connection = StudentUtil.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM studentjdbc");
-
-        List<Student> students = new ArrayList<>();
-        Student stu;
-        while (resultSet.next()) {
-            stu = new Student(resultSet.getString("id")
-                    , resultSet.getString("name")
-                    , resultSet.getString("sex")
-                    , resultSet.getInt("school_time")
-                    , resultSet.getDate("birthday").toString()
-                    , resultSet.getString("class"));
-
-            students.add(stu);
+        Connection connection = DbUtil.getConnection();
+        ResultSet resultSet;
+        try (Statement statement = connection.createStatement()) {
+            resultSet = statement.executeQuery("SELECT * FROM studentjdbc");
+            return getStudents(resultSet);
         }
-        return students;
+
     }
 
     public List<Student> queryByClassId(String classId) throws SQLException {
         // 建立连接
-        Connection connection = StudentUtil.getConnection();
+        Connection connection = DbUtil.getConnection();
         String sql = "SELECT * FROM studentjdbc WHERE class=?";
         // 预编译
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        // 传参
-        preparedStatement.setString(1, classId);
-        // 执行
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        List<Student> students = new ArrayList<>();
-        Student stu;
-        while (resultSet.next()) {
-            stu = new Student(resultSet.getString("id")
-                    , resultSet.getString("name")
-                    , resultSet.getString("sex")
-                    , resultSet.getInt("school_time")
-                    , resultSet.getDate("birthday").toString()
-                    , resultSet.getString("class"));
-
-            students.add(stu);
+        ResultSet resultSet;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            // 传参
+            preparedStatement.setString(1, classId);
+            // 执行
+            resultSet = preparedStatement.executeQuery();
+            return getStudents(resultSet);
         }
-        return students;
+
     }
 
     public void update(String id, Student student) throws SQLException {
@@ -90,36 +71,53 @@ public class StudentRepository {
             throw new notSameIdException();
         }
         // 获取连接
-        Connection connection = StudentUtil.getConnection();
+        Connection connection = DbUtil.getConnection();
         // 构建sql
         String sql = "UPDATE studentjdbc"
                 + " set name=?, sex=?, school_time=?, birthday=?, class=?"
                 + " WHERE id=?";
         // 预编译
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        // 传参
-        preparedStatement.setString(1, student.getName());
-        preparedStatement.setString(2, student.getGender());
-        preparedStatement.setInt(3, student.getAdmissionYear());
-        preparedStatement.setDate(4, new Date(student.getBirthday().getTime())); // 两个Date不一样
-        preparedStatement.setString(5, student.getClassId());
-        preparedStatement.setString(6, id);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            // 传参
+            preparedStatement.setString(1, student.getName());
+            preparedStatement.setString(2, student.getGender());
+            preparedStatement.setInt(3, student.getAdmissionYear());
+            preparedStatement.setDate(4, new Date(student.getBirthday().getTime())); // 两个Date不一样
+            preparedStatement.setString(5, student.getClassId());
+            preparedStatement.setString(6, id);
 
-        // 执行
-        preparedStatement.execute();
+            // 执行
+            preparedStatement.executeUpdate();
+        }
     }
 
     public void delete(String id) throws SQLException {
         // 获取连接
-        Connection connection = StudentUtil.getConnection();
+        Connection connection = DbUtil.getConnection();
         // 构建sql
         String sql = "DELETE FROM studentjdbc WHERE id=?";
         // 预编译
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        // 传参
-        preparedStatement.setString(1, id);
-        // 执行
-        preparedStatement.execute();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            // 传参
+            preparedStatement.setString(1, id);
+            // 执行
+            preparedStatement.executeUpdate();
+        }
+    }
 
+    private List<Student> getStudents(ResultSet resultSet) throws SQLException {
+        List<Student> students = new ArrayList<>();
+        Student stu;
+        while (resultSet.next()) {
+            stu = new Student(resultSet.getString("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("sex"),
+                    resultSet.getInt("school_time"),
+                    resultSet.getDate("birthday").toString(),
+                    resultSet.getString("class"));
+
+            students.add(stu);
+        }
+        return students;
     }
 }
