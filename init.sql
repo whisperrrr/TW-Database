3719e83be9e10540bc49bf44b91e8bfda3f023ee
JinHui <jh731448520@163.com>
2020-04-10 17:40:16 +0800

    [JinHui]{fix}-{#finalTest}:change park_in_time of database to auto update

--- init.sql
+++ init.sql
@@ -1,11 +1,11 @@
 CREATE SCHEMA `parking_lot_sys` DEFAULT CHARACTER SET utf8 ;
 
-CREATE TABLE `parking_lot_sys`.`parking_lot` (
+CREATE TABLE `parking_lot_sys`.`parking_lot_test` (
   `mark` CHAR(2) NOT NULL,
   `id` INT UNSIGNED NOT NULL,
   `is_empty` TINYINT NOT NULL,
   `car_number` VARCHAR(10) NULL,
-  `park_in_time` DATETIME NULL,
+  `park_in_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`mark`, `id`),
   UNIQUE INDEX `car_number_UNIQUE` (`car_number` ASC) VISIBLE)
 ENGINE = InnoDB
